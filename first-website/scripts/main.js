111845bc031d4e20f2cfe920ef4550f19dad8764
jeffrey-weng <41769471+jeffrey-weng@users.noreply.github.com>
2018-07-27 00:01:00 -0400

    Add files via upload

---
+++ first-website/scripts/main.js
@@ -0,0 +1,31 @@
+
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/twisted_fate.jpg') {
+      myImage.setAttribute ('src','images/TwistedFate_2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/twisted_fate.jpg');
+    }
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Welcome to my guide, ' + myName + '!';
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome to my guide, ' + storedName + '!';
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
